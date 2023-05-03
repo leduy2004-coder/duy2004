b14627fb72d6bad4993d504448cb8a68270366b2
leduy2004-coder <leduy8762@gmail.com>
2023-05-03 08:27:03 +0700

    n

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
-window.addEventListener("load", function () {
-    const toggle = document.querySelector(".icon");
-    const menu = document.querySelector(".nav__list");
-    toggle && toggle.addEventListener("click", handleToggleMenu);
-    function handleToggleMenu(e) {
-      menu && menu.classList.add("active");
-    }
-    document.addEventListener("click", handleClickOutside);
-    function handleClickOutside(e) {
-      if (e.target.matches(".icon") || e.target.matches(".nav__list, .nav__list *"))
-        return;
-      menu && menu.classList.remove("active");
+
+  const toggle = document.querySelector('.icon');
+  const menu = document.querySelector('.nav__list');
+  let modal = document.querySelector('.modal');
+  toggle.addEventListener("click", handleToggleMenu);
+  function handleToggleMenu(e) {
+    menu.classList.add("active");
+    modal.classList.add("open");
+  }
+  modal.addEventListener("click", function(e){
+    if (e.target == e.currentTarget){
+      menu.classList.remove("active");
+      modal.classList.remove("open");
     }
   });
 
 
-
-document.getElementById('next').onclick = function () {
+  document.getElementById('next').onclick = function() {
+      const widthItem = document.querySelector('.product__list-item').offsetWidth;
+      document.getElementById('product__contain').scrollLeft += widthItem;
+  }
+  document.getElementById('prev').onclick = function() {
     const widthItem = document.querySelector('.product__list-item').offsetWidth;
-    document.getElementById('product__contain').scrollLeft += widthItem;
-}
-document.getElementById('prev').onclick = function () {
-  const widthItem = document.querySelector('.product__list-item').offsetWidth;
-  document.getElementById('product__contain').scrollLeft -= widthItem;
-}  
+    document.getElementById('product__contain').scrollLeft -= widthItem;
+  }  
 
 
